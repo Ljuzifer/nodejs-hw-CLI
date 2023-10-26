d0ff3721fdd00c6c692a94f7779bc6d3b0cfecdf
Ljuzifer <121060706+Ljuzifer@users.noreply.github.com>
2023-10-27 01:31:28 +0300

    index.js

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
-// const fs = require("node:fs/promises");
-// const gaga = fs.readFile("./db/contacts.json", "utf-8");
-// gaga.then((data) => console.log(data));
+const method = require("./db/contacts");
 
-// function foo(a, b) {
-//   return a + b;
-// }
+const { Command } = require("commander");
+const program = new Command();
 
-// console.log(foo(8, 8));
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const contacts = method.listContacts();
+      return console.table(contacts);
+
+    case "get":
+      const contact = method.getContactById(id);
+      return console.log(contact);
+
+    case "add":
+      const newContact = method.addContact(name, email, phone);
+      return console.log(newContact);
+
+    case "remove":
+      const deleted = method.removeContact(id);
+      return console.log(deleted);
+
+    default:
+      return console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+invokeAction(argv);
